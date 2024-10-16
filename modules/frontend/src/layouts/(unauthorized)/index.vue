efe03e5ea1dcadf72ccf1cbe08898d9412489826
Artem Zaitsev <Aldeimeter666@gmail.com>
2024-10-16 13:09:45 +0200

    feat: added register page

--- modules/frontend/src/layouts/(unauthorized)/index.vue
+++ modules/frontend/src/layouts/(unauthorized)/index.vue
@@ -1,6 +1,6 @@
 <template>
   <main id="page" class="row justify-center">
-    <form class="form column flex-center shadow-2 col-10 col-sm-6 col-md-4 col-lg-2 self-center">
+    <form class="form column flex-center shadow-2 col-10 col-sm-6 col-md-4 col-lg-3 self-center">
       <div class="logo column">
         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none"
           stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round"
---
+++ modules/frontend/src/pages/(unauthorized)/register/index.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+import { reactive, ref } from 'vue';
+const form = reactive({
+  email: '',
+  username: '',
+  fullname: '',
+  password: ''
+})
+const errorMessage = ref('')
+const onSubmit = () => {
+  console.log(form)
+}
+</script>
+
+<template>
+  <span class="auth-title">Sign up to Slash</span>
+  <input class="q-input" type="email" required v-model="form.email" placeholder="Email" />
+  <input class="q-input" type="text" required v-model="form.username" placeholder="Username" />
+  <input class="q-input" type="text" required v-model="form.fullname" placeholder="Full name" />
+  <input class="q-input" type="password" required v-model="form.password" placeholder="Password" />
+  <button class="q-btn" type="button" @click="onSubmit">Sign up</button>
+  <div class="auth-error-message" v-if="errorMessage">{{ errorMessage }}</div>
+  <div class="auth-footer-text">Already have an account? <a href="#/auth/login">Sign in</a></div>
+</template>
+
+<style scoped>
+.q-btn {
+  width: 100%;
+}
+</style>
